eb1ab756e411fac26e4fcc0bc04242eba980b543
smirnov <smirnov@alfasatcom.ru>
2024-08-02 13:42:00 +0300

    add router to project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Paths } from './paths'
+import User from './pages/user'
+import './index.css'
+
+
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    // element: <h1>home</h1>
+    element: <User />
+  },
+  {
+    path: Paths.user,
+    element: <h1>user</h1>
+  },
+])
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
+      {/* <App /> */}
     </Provider>
   </React.StrictMode>
 );
@@ -19,4 +38,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ client/src/paths.ts
@@ -0,0 +1,6 @@
+export const Paths = {
+  home: '/',
+  userAdd: '/user/add',
+  userEdit: '/user/edit',
+  user: '/user',
+} 
